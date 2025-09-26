97578dceaa59ce9ad6ceda83db21e663d1b39303
Inidapaa <139908461+Inidapaa@users.noreply.github.com>
2025-09-26 22:57:46 +0700

    Refactor Checkout component: improved structure and formatting for better readability and consistency

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -1,125 +1,151 @@
-import Navbar from "../components/navbar"
-import Title from "../components/title"
+import Navbar from "../components/navbar";
+import Title from "../components/title";
 
 const Checkout = () => {
   return (
     <>
-    <Navbar/>
-    <Title/>
-    {/* ==========Daftar Pesanan========== */}
-    <div className="h-auto w-screen flex justify-center items-center">
-      <div className="md:flex flex-wrap justify-center max-w-[80vw] my-10 gap-x-10">
-        <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
-          <img src="" alt="" />
-          <div className="flex flex-col">
-            <h3 className="font-bold">Cappucino</h3>
-            <p>300ml</p>
-            <p className="font-bold text-[0.9rem]">Rp 10.000</p>
-          </div>
-          <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
-            <button type="" className="cursor-pointer">+</button>
-            <p className="text-[0.8rem]">2</p>
-            <button type="" className="cursor-pointer">-</button>
-          </div>
-        </div>
-        <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
-          <img src="" alt="" />
-          <div className="flex flex-col">
-            <h3 className="font-bold">Cappucino</h3>
-            <p>300ml</p>
-            <p className="font-bold text-[0.9rem]">Rp 10.000</p>
-          </div>
-          <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
-            <button type="" className="cursor-pointer">+</button>
-            <p className="text-[0.8rem]">2</p>
-            <button type="" className="cursor-pointer">-</button>
-          </div>
-        </div>
-        <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
-          <img src="" alt="" />
-          <div className="flex flex-col">
-            <h3 className="font-bold">Cappucino</h3>
-            <p>300ml</p>
-            <p className="font-bold text-[0.9rem]">Rp 10.000</p>
-          </div>
-          <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
-            <button type="" className="cursor-pointer">+</button>
-            <p className="text-[0.8rem]">2</p>
-            <button type="" className="cursor-pointer">-</button>
-          </div>
-        </div>
-        <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
-          <img src="" alt="" />
-          <div className="flex flex-col">
-            <h3 className="font-bold">Cappucino</h3>
-            <p>300ml</p>
-            <p className="font-bold text-[0.9rem]">Rp 10.000</p>
-          </div>
-          <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
-            <button type="" className="cursor-pointer">+</button>
-            <p className="text-[0.8rem]">2</p>
-            <button type="" className="cursor-pointer">-</button>
-          </div>
-        </div>
-        <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
-          <img src="" alt="" />
-          <div className="flex flex-col">
-            <h3 className="font-bold">Cappucino</h3>
-            <p>300ml</p>
-            <p className="font-bold text-[0.9rem]">Rp 10.000</p>
-          </div>
-          <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
-            <button type="" className="cursor-pointer">+</button>
-            <p className="text-[0.8rem]">2</p>
-            <button type="" className="cursor-pointer">-</button>
-          </div>
-        </div>
-        <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
-          <img src="" alt="" />
-          <div className="flex flex-col">
-            <h3 className="font-bold">Cappucino</h3>
-            <p>300ml</p>
-            <p className="font-bold text-[0.9rem]">Rp 10.000</p>
-          </div>
-          <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
-            <button type="" className="cursor-pointer">+</button>
-            <p className="text-[0.8rem]">2</p>
-            <button type="" className="cursor-pointer">-</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-    {/* ========Ringkasan Pembayaran========== */}
-    <h1 className="ml-7 md:text-3xl text-2xl font-bold md:text-center text-start">Ringkasan Pembayaran</h1>
-    <div className="h-90 w-full my-10 px-9 md:px-10">
-      <div className=" mx-auto flex justify-center items-center h-auto pb-10">
-        <div className="grid md:grid-cols-2 gap-10 w-5xl">
-          <div className="bg-white shadow-custom rounded-3xl flex justify-between items-center h-30 p-5">
-            <h1 className="text-2xl">Total Pembayaran</h1>
+      <Navbar />
+      <Title />
+      {/* ==========Daftar Pesanan========== */}
+      <div className="h-auto w-screen flex justify-center items-center">
+        <div className="md:flex flex-wrap justify-center max-w-[80vw] my-10 gap-x-10">
+          <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
+            <img src="" alt="" />
             <div className="flex flex-col">
-              <p className="text-center text-2xl">36.000</p>
-              <span className="flex flex-col items-end text-gray-400">
-                <p className="text-[14px]">10.000 x 2</p>
-                <p className="text-[12px]">8.00 x 2</p>
-              </span>
+              <h3 className="font-bold">Cappucino</h3>
+              <p>300ml</p>
+              <p className="font-bold text-[0.9rem]">Rp 10.000</p>
+            </div>
+            <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
+              <button type="" className="cursor-pointer">
+                +
+              </button>
+              <p className="text-[0.8rem]">2</p>
+              <button type="" className="cursor-pointer">
+                -
+              </button>
             </div>
           </div>
-          <div className="bg-white shadow-custom rounded-3xl flex justify-between items-center h-30 p-5">
-            <h1 className="text-2xl">Total Pembayaran</h1>
+          <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
+            <img src="" alt="" />
             <div className="flex flex-col">
-              <p className="text-center text-2xl">36.000</p>
-              <span className="flex flex-col items-end text-gray-400">
-                <p className="text-[14px]">10.000 x 2</p>
-                <p className="text-[12px]">8.00 x 2</p>
-              </span>
+              <h3 className="font-bold">Cappucino</h3>
+              <p>300ml</p>
+              <p className="font-bold text-[0.9rem]">Rp 10.000</p>
+            </div>
+            <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
+              <button type="" className="cursor-pointer">
+                +
+              </button>
+              <p className="text-[0.8rem]">2</p>
+              <button type="" className="cursor-pointer">
+                -
+              </button>
+            </div>
+          </div>
+          <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
+            <img src="" alt="" />
+            <div className="flex flex-col">
+              <h3 className="font-bold">Cappucino</h3>
+              <p>300ml</p>
+              <p className="font-bold text-[0.9rem]">Rp 10.000</p>
+            </div>
+            <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
+              <button type="" className="cursor-pointer">
+                +
+              </button>
+              <p className="text-[0.8rem]">2</p>
+              <button type="" className="cursor-pointer">
+                -
+              </button>
+            </div>
+          </div>
+          <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
+            <img src="" alt="" />
+            <div className="flex flex-col">
+              <h3 className="font-bold">Cappucino</h3>
+              <p>300ml</p>
+              <p className="font-bold text-[0.9rem]">Rp 10.000</p>
+            </div>
+            <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
+              <button type="" className="cursor-pointer">
+                +
+              </button>
+              <p className="text-[0.8rem]">2</p>
+              <button type="" className="cursor-pointer">
+                -
+              </button>
+            </div>
+          </div>
+          <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
+            <img src="" alt="" />
+            <div className="flex flex-col">
+              <h3 className="font-bold">Cappucino</h3>
+              <p>300ml</p>
+              <p className="font-bold text-[0.9rem]">Rp 10.000</p>
+            </div>
+            <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
+              <button type="" className="cursor-pointer">
+                +
+              </button>
+              <p className="text-[0.8rem]">2</p>
+              <button type="" className="cursor-pointer">
+                -
+              </button>
+            </div>
+          </div>
+          <div className="bg-white mb-10 h-25 w-80 border-2 rounded-3xl flex place-content-between items-center">
+            <img src="" alt="" />
+            <div className="flex flex-col">
+              <h3 className="font-bold">Cappucino</h3>
+              <p>300ml</p>
+              <p className="font-bold text-[0.9rem]">Rp 10.000</p>
+            </div>
+            <div className="flex h-20 w-7 mr-2 rounded-3xl flex-col bg-secondary justify-center items-center text-white">
+              <button type="" className="cursor-pointer">
+                +
+              </button>
+              <p className="text-[0.8rem]">2</p>
+              <button type="" className="cursor-pointer">
+                -
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    </>
-  )
-}
 
-export default Checkout
+      {/* ========Ringkasan Pembayaran========== */}
+      <h1 className="ml-7 md:text-3xl text-2xl font-bold md:text-center text-start">
+        Ringkasan Pembayaran
+      </h1>
+      <div className="h-90 w-full my-10 px-9 md:px-10">
+        <div className=" mx-auto flex justify-center items-center h-auto pb-10">
+          <div className="grid md:grid-cols-2 gap-10 w-5xl">
+            <div className="bg-white shadow-custom rounded-3xl flex justify-between items-center h-30 p-5">
+              <h1 className="text-2xl">Total Pembayaran</h1>
+              <div className="flex flex-col">
+                <p className="text-center text-2xl">36.000</p>
+                <span className="flex flex-col items-end text-gray-400">
+                  <p className="text-[14px]">10.000 x 2</p>
+                  <p className="text-[12px]">8.00 x 2</p>
+                </span>
+              </div>
+            </div>
+            <div className="bg-white shadow-custom rounded-3xl flex justify-between items-center h-30 p-5">
+              <h1 className="text-2xl">Total Pembayaran</h1>
+              <div className="flex flex-col">
+                <p className="text-center text-2xl">36.000</p>
+                <span className="flex flex-col items-end text-gray-400">
+                  <p className="text-[14px]">10.000 x 2</p>
+                  <p className="text-[12px]">8.00 x 2</p>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
